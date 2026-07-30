f709e4ebbd1d08836d4eb8a3b3cf6029319c6a7d
Manohar <40585757+mmukidi@users.noreply.github.com>
2026-07-30 13:28:01 -0400

    Enforce uniform dark background (#0f0f13) across native iOS Window, html, body, and status bar

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window?.backgroundColor = UIColor(red: 15/255, green: 15/255, blue: 19/255, alpha: 1.0)
         return true
     }
 
